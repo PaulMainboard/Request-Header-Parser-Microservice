5bac4ed9a5a50acda31eafb60293ad9bfee3edf8
PaulMainboard <paulwilliams669@gmail.com>
2017-12-09 03:32:17 -0500

    Have program display all the information that the server collected.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Print HTTP header information and other info that the server collected ** For Testing Purposes **
+foreach ($_SERVER as $key => $value) {
+    echo $key . " : " . $value . "<br>";
+}
+
+// Find the languange of the browser.
+echo "<br>" . substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, strpos($_SERVER['HTTP_ACCEPT_LANGUAGE'], ","));
+
+// Find the OS of the guest computer.
+preg_match("/\(([^)]+)\)/", $_SERVER['HTTP_USER_AGENT'], $os);
+echo "<br>" . 
+    $os[1];
