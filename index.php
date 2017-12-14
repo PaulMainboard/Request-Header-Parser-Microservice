16dd404c93b93f4cfca032edd9463379e31ac76c
PaulMainboard <paulwilliams669@gmail.com>
2017-12-13 22:37:35 -0500

    Added composer.json file for Heroku deployment.

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $headerObj->ipAddress = $_SERVER['REMOTE_ADDR'];
 // Get the language of the browser of the device
 $fullLangaugeText = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 $language =  substr($fullLanguageText, 0, strpos($fullLanguageText, ","));
+$headerObj->language = $language;
 
 // Get the OS name of the guest computer.
 preg_match("/\(([^)]+)\)/", $_SERVER['HTTP_USER_AGENT'], $os);
